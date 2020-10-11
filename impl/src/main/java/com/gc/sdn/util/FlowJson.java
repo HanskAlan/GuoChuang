8b5aaf29650dca45ef2fee5b6068a839e1a02e0f
ABerrorch <784031469@qq.com>
2020-10-11 14:12:56 +0800

    在h1->h7传输中成功地下发流表，但是路径上的C2节点居然没有接收到流表和meter表

--- impl/src/main/java/com/gc/sdn/util/FlowJson.java
+++ impl/src/main/java/com/gc/sdn/util/FlowJson.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 /**
  * @Description:
  * @author: pan.wen
- * @date:2020/7/17 16:08
+ * @date: 2020/7/17 16:08
  * @Name: FlowJson
  */
 @Service
@@ -24,56 +24,97 @@ public class FlowJson {
         jsonObjectInventory.put("flow-name","");
         jsonObjectInventory.put("cookie",256);
 
-        JSONObject jsonObjectOutputAction = new JSONObject();
-        jsonObjectOutputAction.put("output-node-connector",output);
 
-        JSONObject jsonObjectAction = new JSONObject();
-        jsonObjectAction.put("order",0);
-        jsonObjectAction.put("output-action",jsonObjectOutputAction);
+        /* 下面需要的是这种形式
+         * <instructions>
+         *     <instruction>
+         *         <order>0</order>
+     *             <meter>
+     *                 <meter-id>1</meter-id>
+     *             </meter>
+         *     </instruction>
+         *     <instruction>
+         *         <order>1</order>
+         *         <apply-actions>
+         *             <action>
+             *             <order>1</order>
+             *             <output-action>
+             *                 <output-node-connector>2</output-node-connector>
+             *             </output-action>
+         *             </action>
+         *         </apply-actions>
+         *     </instruction>
+         * </instructions>
+         */
 
-        JSONArray jsonArrayAction = new JSONArray();
-        jsonArrayAction.add(jsonObjectAction);
+//        "instructions": {
+//            "instruction": [
+//                {
+//                    "order": 0,
+//                    "apply-actions": {
+//                        "action": [
+//                            {
+//                                "order": 0,
+//                                "dec-nw-ttl": {}
+//                            }
+//                        ]
+//                    }
+//                }
+//            ]
+//        }
+//
+        // 设置整体状态
+        JSONObject meterInstruction = new JSONObject();
+        JSONObject actionsInstruction = new JSONObject();
+        jsonObjectInventory.put("instructions", new JSONObject()
+                .fluentPut("instruction", new JSONArray()
+                        .fluentAdd(meterInstruction)
+                        .fluentAdd(actionsInstruction)
+                )
+        );
+        // meter 的 instruction
+        meterInstruction
+                .fluentPut("order",0)
+                .fluentPut("meter",new JSONObject()
+                        .fluentPut("meter-id",String.valueOf(co_flow_id) + flow_id)
+                );
 
-        JSONObject jsonObjectApplyActions = new JSONObject();
-        jsonObjectApplyActions.put("action",jsonArrayAction);
+        // apply-action 的 instruction
+        actionsInstruction
+                .fluentPut("order",1)
+                .fluentPut("apply-actions",new JSONObject()
+                        .fluentPut("action",new JSONArray()
+                                .fluentAdd(new JSONObject()
+                                        .fluentPut("order",1)
+                                        .fluentPut("output-action",new JSONObject()
+                                                .fluentPut("output-node-connector",output)
+                                        )
+                                )
+                        )
+                );
 
-        JSONObject jsonObjectInstruction = new JSONObject();
-        jsonObjectInstruction.put("order",0);
-        jsonObjectInstruction.put("apply-actions",jsonObjectApplyActions);
 
-        JSONObject jsonMeterId = new JSONObject();
-        jsonMeterId.put("meter-id",String.valueOf(co_flow_id)+flow_id);
 
-        JSONObject jsonMeter = new JSONObject();
-        jsonMeter.put("order",0);
-        jsonMeter.put("meter",jsonMeterId);
-
-        JSONArray jsonArrayInstruction = new JSONArray();
-        jsonArrayInstruction.add(jsonObjectInstruction);
-        jsonArrayInstruction.add(jsonMeter);
-
-        JSONObject jsonObjectInstructions = new JSONObject();
-        jsonObjectInstructions.put("instruction",jsonArrayInstruction);
-
-        jsonObjectInventory.put("instructions",jsonObjectInstructions);
 
         jsonObjectInventory.put("priority",666);
         jsonObjectInventory.put("table_id",0);
-
-        JSONObject jsonObjectMatch = new JSONObject();
-        jsonObjectMatch.put("ipv4-destination",target+"/32");
-        jsonObjectMatch.put("in-port",in_port);
-
-        JSONObject jsonObjectEthernetType = new JSONObject();
-        jsonObjectEthernetType.put("type",2048);
-
-        JSONObject jsonObjectEthernetMatch = new JSONObject();
-        jsonObjectEthernetMatch.put("ethernet-type",jsonObjectEthernetType);
-
-        jsonObjectMatch.put("ethernet-match",jsonObjectEthernetMatch);
-
-        jsonObjectInventory.put("match",jsonObjectMatch);
-
+        // 匹配项
+        jsonObjectInventory.put("match", new JSONObject()
+                .fluentPut("ipv4-destination", target + "/32") // 额外的过滤项
+                .fluentPut("in-port", in_port) // 额外的过滤项
+                .fluentPut("udp-source-port",65535 - (60 * co_flow_id + flow_id)) // 这个是核心
+                .fluentPut("udp-destination-port",5001) // 这个也是
+                .fluentPut("ethernet-match", new JSONObject() // 这个是必须的
+                        .fluentPut("ethernet-type",new JSONObject()
+                                .fluentPut("type",2048)
+                        )
+                )
+                .fluentPut("ip-match",new JSONObject() // 这个也是必须的，不知道为什么
+                        .fluentPut("ip-protocol",17)
+//                        .fluentPut("ip-dscp",8)
+//                        .fluentPut("ip-ecn",3)
+                )
+        );
         jsonArrayInventory.add(jsonObjectInventory);
 
         jsonObjectAll.put("flow-node-inventory:flow",jsonArrayInventory);
