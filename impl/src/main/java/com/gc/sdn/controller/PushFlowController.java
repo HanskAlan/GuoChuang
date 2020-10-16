b1fe68ca738ae73d53b5f5ff3ed36b11af9d180e
ABerrorch <784031469@qq.com>
2020-10-16 12:14:38 +0800

    修改异常机制 优化PushFlowController的速度

--- impl/src/main/java/com/gc/sdn/controller/PushFlowController.java
+++ impl/src/main/java/com/gc/sdn/controller/PushFlowController.java
@@ -7,71 +7,115 @@ import com.gc.sdn.constant.Constant;
 import com.gc.sdn.constant.ResponseResult;
 import com.gc.sdn.service.PushFlowService;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 import static com.gc.sdn.util.ParameterUtil.*;
 
 
 public class PushFlowController {
     public static ResponseResult startRacPushFlow(JSONArray jsonArray){
         try {
-            //解析开始
-            for(int i=0; i<jsonArray.size(); i++){
-                //协流id
+            // 第一层，遍历协流的Assignment
+            for(int i = 0; i < jsonArray.size(); i++){
                 int coflowId = jsonArray.getJSONObject(i).getIntValue("coflowID");
-                //协流属性
-                JSONArray jsonArrayFlow = jsonArray.getJSONObject(i).getJSONArray("flowAssignments");
-                for(int j=0; j < jsonArrayFlow.size(); j++){
-                    JSONObject jsonObjectFlow = jsonArrayFlow.getJSONObject(j);
-                    // 子流id
-                    int flowId = jsonObjectFlow.getIntValue("flowID");
-//                    //源节点
-//                    String srcName = jsonObjectFlow.getString("source");
-                    //目标节点
-                    String dstName = jsonObjectFlow.getString("target");
-//                    //流大小
-//                    Long size = jsonObjectFlow.getLong("size");
-                    //传输速率
-                    Double rate = jsonObjectFlow.getDouble("rate");
-//                    //路径长度
-//                    int path_length = jsonObjectFlow.getIntValue("path-length");
-                    //路径过程属性
-                    JSONArray jsonArrayPathProcess = jsonObjectFlow.getJSONArray("pathProcess");
-                    //迭代
-                    if(jsonArrayPathProcess.size() < 3){
-                        throw new Exception("RAC给出的路径信息长度小于3，不是合法的路径信息");
-                    }
-                    for (int p = 0; p + 2 < jsonArrayPathProcess.size(); p++) {
-                        // 以一个三元组（A，B，C）作为一次下发流表的输入，其中B是下发流表的交换机
-                        // A和C有可能是主机，也有可能是交换机
-                        String A = jsonArrayPathProcess.getString(p);
-                        String B = jsonArrayPathProcess.getString(p + 1);
-                        String C = jsonArrayPathProcess.getString(p + 2);
-                        // 预处理A，A可能是主机
-                        if(p == 0) A = nodeMap.get(A);
-                        // 预处理C，C可能是主机
-                        if(p + 3 == jsonArrayPathProcess.size()) C = nodeMap.get(C);
+                JSONArray flowAssignments = jsonArray.getJSONObject(i).getJSONArray("flowAssignments");
 
-                        String[] splitBuff;
-                        // 计算B的输入端口
-                        splitBuff = portMap.get(A + "-" + B).split("-");
-                        String inPort = splitBuff[1].split(":")[2];
-//                        inPort = extend(B,inPort);
+                // 第二层，遍历流Assignment
+                for(int j = 0; j < flowAssignments.size(); j++){
+                    try {
+                        JSONObject jsonObjectFlow = flowAssignments.getJSONObject(j);
+                        int flowId = jsonObjectFlow.getIntValue("flowID");
+                        String dstName = jsonObjectFlow.getString("target");
+                        Double rate = jsonObjectFlow.getDouble("rate");
+                        JSONArray pathProcess = jsonObjectFlow.getJSONArray("pathProcess");
 
-                        // 计算B的输出端口
-                        splitBuff = portMap.get(B + "-" + C).split("-");
-                        String outPort = splitBuff[0].split(":")[2];
-//                        outPort = extend(B,outPort);
+                        if(pathProcess.size() < 3){
+                            throw new Exception("RAC给出的路径信息长度小于3，不是合法的路径信息");
+                        }
+                        String A = null;
+                        String B = nodeMap.get(pathProcess.getString(0));
+                        String C = pathProcess.getString(1);
 
-                        PushFlowService.pushFlowAndMeter(coflowId,flowId, rate, inPort, outPort, B, dstName, p);
+                        for (int p = 0; p + 2 < pathProcess.size(); p++) {
+                            // 以一个三元组（A，B，C）作为一次下发流表的输入，其中B是下发流表的交换机
+                            A = B;B = C;C = pathProcess.getString(p + 2);
+                            // 预处理C，C可能是主机
+                            if(p + 3 == pathProcess.size()) C = nodeMap.get(C);
+
+//                            String inPort = getInPort(A,B); // inPort_A -> outPort_B
+                            String outPort = getOutPort(B,C); // inPort_B -> outPort_C
+
+                            PushFlowService.pushFlowAndMeter(coflowId,flowId, rate, outPort, B, dstName, p);
+                        }
+                    } catch (Exception e){
+                        // 我也不知道到底是哪里爆了异常
+                        // 但是至少让一个flow的异常不要影响到另外一个flow
+                        System.out.println("startRacPushFlow Exception0:");
+                        System.out.println(flowAssignments.getJSONObject(j).toJSONString());
+                        e.printStackTrace();
                     }
                 }
             }
             return new ResponseResult(Constant.RESULT_SUCCESS_CODE, Constant.RESULT_SUCCESS_DESC);
         } catch (JSONException e){
+            System.out.println("startRacPushFlow Exception1:");
+            e.printStackTrace();
             return new ResponseResult(Constant.FORMAT_ERROR,Constant.RESULT_FAIL_DESC);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
+            System.out.println("startRacPushFlow Exception2:");
+            e.printStackTrace();
             return new ResponseResult(Constant.RESULT_FAIL_CODE, Constant.RESULT_FAIL_DESC);
         }
     }
 
+    private static final HashMap<String, HashMap<String,String[]>> quickPortMap = new HashMap<>();
+    private static String[] getPort(String A,String B){
+        if(quickPortMap.containsKey(A)){
+            if(quickPortMap.get(A).containsKey(B))
+                return quickPortMap.get(A).get(B);
+        } else quickPortMap.put(A,new HashMap<>());
 
+        String[] ports = {"initial","initial"};
+
+        String[] splitBuff = null,temp;
+        try{ // A:output -> B:input
+            splitBuff = portMap.get(A + "-" + B).split("-");
+            temp = splitBuff[0].split(":");
+            if(temp.length == 3) ports[0] = temp[2]; // output
+
+            temp = splitBuff[1].split(":");
+            if(temp.length == 3) ports[1] = temp[2]; // input
+        } catch (Exception e){
+            try { // 如果没有，查看反向的情形有没有 B:input <- A:output
+                splitBuff = portMap.get(B + "-" + A).split("-");
+                temp = splitBuff[0].split(":");
+                if(temp.length == 3) ports[1] = temp[2]; // input
+
+                temp = splitBuff[1].split(":");
+                if(temp.length == 3) ports[0] = temp[2]; // output
+            } catch (Exception e1){
+                ports[0] = ports[1] = "Error";
+                System.out.println("getPort Exception: A = " + A + ", B = " + B );
+                System.out.println(Arrays.toString(splitBuff));
+                e.printStackTrace();
+                e1.printStackTrace();
+            }
+        }
+
+        quickPortMap.get(A).put(B,ports);
+        return ports;
+    }
+
+    /**
+     * 注意端口是从outPort -> inPort 的，返回的是A是outPort，B的inPort
+     */
+    public static String getInPort(String A,String B){
+        return getPort(A,B)[1];
+    }
+
+    public static String getOutPort(String A,String B){
+        return getPort(A,B)[0];
+    }
 }
--- impl/src/main/java/com/gc/sdn/service/PushFlowService.java
+++ impl/src/main/java/com/gc/sdn/service/PushFlowService.java
@@ -13,22 +13,27 @@ public class PushFlowService {
     public static final String FLOW_URI = "http://" + Constant.host + ":8181/restconf/operations/sal-flow:add-flow";
     public static final String METER_URI = "http://" + Constant.host + ":8181/restconf/operations/sal-meter:add-meter";
 
-    public static void pushFlowAndMeter(int coflowId, int flowId, Double rate, String inPort, String outPort, String nodeName, String dstIp, int p){
-        // 初始化odl工具类用于登陆的权限验证 初始话的时候运行了一些东西，不知道有什么用
-        @SuppressWarnings("unused") OdlUtil odlUtil = new OdlUtil(Constant.host,Constant.port,Constant.username,Constant.password,Constant.containerName);
-        int bandwidth = new Double(rate * 8000).intValue();
-        // 调用odl接口下发meter表
-        JSONObject jsonObjectMeter = MeterJson.getMeterJson(coflowId,bandwidth,flowId,nodeName,p);
-        OdlUtil.installRPC(
-                jsonObjectMeter.toJSONString(),
-                METER_URI
-        );
+    public static void pushFlowAndMeter(int coflowId, int flowId, Double rate, String outPort, String nodeName, String dstIp, int p){
+        try{
+            // 初始化odl工具类用于登陆的权限验证 初始话的时候运行了一些东西，不知道有什么用
+            @SuppressWarnings("unused") OdlUtil odlUtil = new OdlUtil(Constant.host,Constant.port,Constant.username,Constant.password,Constant.containerName);
+            int bandwidth = new Double(rate * 8000).intValue();
+            // 调用odl接口下发meter表
+            JSONObject meter = MeterJson.getMeterJson(coflowId,bandwidth,flowId,nodeName,p);
+            OdlUtil.installRPC(
+                    meter.toJSONString(),
+                    METER_URI
+            );
 
-        // 调用odl接口下发流
-        OdlUtil.installRPC(
-                new FlowJson(coflowId,flowId,dstIp,nodeName).transmitAction(outPort).getRPCFlowTable(),
-                FLOW_URI
-        );
+            // 调用odl接口下发流
+            OdlUtil.installRPC(
+                    new FlowJson(coflowId,flowId,dstIp,nodeName).transmitAction(outPort).getRPCFlowTable(),
+                    FLOW_URI
+            );
+        } catch (Exception e){
+            System.out.println("pushFlowAndMeter Exception:");
+            e.printStackTrace();
+        }
     }
 
     public static void pushDropFlow(int co_flow_id, int flow_id, String srcIp, String dstIp){
